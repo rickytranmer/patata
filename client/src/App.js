543ea199b5ef7fa76ca2b40bdd82f662e52db886
rickytranmer <rwtranmer@gmail.com>
2018-02-22 10:40:50 -0700

    test back-end functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.test }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/test');
+    const body = await response.json();
+    console.log(body);
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Patata - Tasks &amp; Timers</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.state.response}</p>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.static(__dirname + '/public'));
+app.use(require('helmet')());
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+// - Routes
 app.get('/api/test', (req, res) => {
   res.send({ test: 'success' });
 });
 
-app.listen(port, ()=> console.log(`Listening on port ${port}`));
+app.listen(port, ()=> {
+	console.log((new Date()).toString());
+	console.log(`- Listening on port ${port}`);
+});
