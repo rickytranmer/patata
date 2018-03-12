9784103c1c97259933eb765d751967010f24bd66
rickytranmer <rwtranmer@gmail.com>
2018-03-12 13:50:07 -0600

    version

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,7 @@ const tasksController = require('../controllers/tasks');
 const usersController = require('../controllers/users');
 
 router.get('/api/test', (req, res)=> {
-  res.send({ test: ' server: 3.12c' });
+  res.send({ test: ' server: 3.12C' });
 });
 
 router.route('/api/task')
