accff27b7d2bd99c1cbecb984add6a345b88f3ad
rickytranmer <rwtranmer@gmail.com>
2018-03-12 15:40:54 -0600

    success redirect

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,7 @@ const table = "Users";
 
 function postSignup(req, res, next) {
 	let signupStrategy = passport.authenticate('local-signup', {
-		successRedirect: 	'/',
+		successRedirect: 	'https://rickytranmer.github.io/patata/',
 		failureRedirect: 	'https://rickytranmer.github.io/patata/signup'
 	});
 	return signupStrategy(req, res, next);
