9e7d64d1f15edb4a996f223bc301649f837f3133
rickytranmer <rwtranmer@gmail.com>
2018-03-12 16:04:23 -0600

    test route removal

--- config/routes.js
+++ config/routes.js
@@ -2,9 +2,6 @@ const router = require('express').Router();
 const tasksController = require('../controllers/tasks');
 const usersController = require('../controllers/users');
 
-router.get('/favicon.ico', (req, res)=> {
-	res.sendFile('./favicon.ico');
-});
 
 router.get('/api/test', (req, res)=> {
   res.send({ test: ' server: 3.12f' });
@@ -22,7 +19,7 @@ router.get('/api/tasks', tasksController.getTasks);
 router.get('/api/tasks/:username', tasksController.getTasks);
 
 router.route('/api/user')
-	.get((req, res)=> {	res.redirect('https://rickytranmer.github.io/patata') })
+	// .get((req, res)=> {	res.redirect('https://rickytranmer.github.io/patata') })
 	.post(usersController.postSignup);
 
 router.get('/*', (req, res)=> {
