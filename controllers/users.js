50bc3b26f00ebe6dbf95e7fa4b85c38ad820831f
rickytranmer <rwtranmer@gmail.com>
2018-03-12 16:36:50 -0600

    revert

--- controllers/users.js
+++ controllers/users.js
@@ -4,13 +4,10 @@ const table = "Users";
 
 
 function postSignup(req, res, next) {
-	let signupStrategy = passport.authenticate('local-signup'), (req, res)=> {
-  	res.send({ username: req.user.username });
-	};
-	// {
-	// 	successRedirect: 	'https://rickytranmer.github.io/patata/',
-	// 	failureRedirect: 	'https://rickytranmer.github.io/patata/signup'
-	// });
+	let signupStrategy = passport.authenticate('local-signup', {
+		successRedirect: 	'https://rickytranmer.github.io/patata/',
+		failureRedirect: 	'https://rickytranmer.github.io/patata/signup'
+	});
 	return signupStrategy(req, res, next);
 }
 
